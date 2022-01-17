939da76a5a8d43577dddca799adfc7d62b8a3d9a
Dev Raj Gaur <devrajgaur786@gmail.com>
2022-01-18 03:17:16 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/delhi_metro_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.delhi_metro_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Delhi Metro App'),
+          backgroundColor: Colors.red[500],
+        ),
+        body: Center(),
+      ),
+    ),
+  );
+}
